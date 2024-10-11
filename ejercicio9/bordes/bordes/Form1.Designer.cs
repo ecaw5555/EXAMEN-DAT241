cf1187ee996b765f4b0a002c977d262dce3e854d
ELIZABETH CUSI ANCASI <vava62001@gmail.com>
2024-10-10 23:07:00 -0400

    Add files via upload

---
+++ ejercicio9/bordes/bordes/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace bordes
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(197, 296);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(284, 12);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(195, 285);
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(497, 97);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "seleciona";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(584, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ ejercicio9/bordes/bordes/Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace bordes
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+
+      
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.Filter = "Image Files|*.jpg;*.jpeg;*.png;*.bmp";
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    Bitmap originalImage = new Bitmap(openFileDialog.FileName);
+                    pictureBox1.Image = originalImage;
+
+                    Bitmap edgeImage = DetectEdges(originalImage);
+                    pictureBox2.Image = edgeImage;
+                }
+            }
+        }
+
+        private Bitmap DetectEdges(Bitmap original)
+        {
+            Bitmap edges = new Bitmap(original.Width, original.Height);
+
+         
+            for (int y = 0; y < original.Height; y++)
+            {
+                for (int x = 0; x < original.Width; x++)
+                {
+                    Color pixel = original.GetPixel(x, y);
+                    int gray = (int)(pixel.R * 0.299 + pixel.G * 0.587 + pixel.B * 0.114);
+                    edges.SetPixel(x, y, Color.FromArgb(gray, gray, gray));
+                }
+            }
+
+      
+            for (int y = 1; y < edges.Height - 1; y++)
+            {
+                for (int x = 1; x < edges.Width - 1; x++)
+                {
+                    int gx = (edges.GetPixel(x - 1, y - 1).R + 2 * edges.GetPixel(x, y - 1).R + edges.GetPixel(x + 1, y - 1).R) -
+                              (edges.GetPixel(x - 1, y + 1).R + 2 * edges.GetPixel(x, y + 1).R + edges.GetPixel(x + 1, y + 1).R);
+
+                    int gy = (edges.GetPixel(x - 1, y - 1).R + 2 * edges.GetPixel(x - 1, y).R + edges.GetPixel(x - 1, y + 1).R) -
+                              (edges.GetPixel(x + 1, y - 1).R + 2 * edges.GetPixel(x + 1, y).R + edges.GetPixel(x + 1, y + 1).R);
+
+                    int g = (int)Math.Sqrt(gx * gx + gy * gy);
+                    g = g > 255 ? 255 : g;
+
+                    edges.SetPixel(x, y, Color.FromArgb(g, g, g));
+                }
+            }
+
+            return edges;
+        }
+
+     
+    }
+}
